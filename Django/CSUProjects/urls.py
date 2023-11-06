05caef0c1e01fb4ee43797d4046e2b733e70094f
Felix Zeves <karpukov2002@mail.ru>
2023-11-06 19:31:51 +0500

    upd: urls.css

--- Django/CSUProjects/urls.py
+++ Django/CSUProjects/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.main, name="main"),
     path('subProjects/<int:project_id>', views.subProjects, name='subProjects'),
     path('completedProjects', views.completedProjects, name='completedProjects'),
-    path('cinema', views.cinema, name='cinema'),
+    path('cinema/<int:comp_project_id>', views.cinema, name='cinema'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
