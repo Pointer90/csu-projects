8c107f575633747540fcc6bc1bd2f35e0ac8293e
Felix Zeves <karpukov2002@mail.ru>
2023-11-07 20:15:57 +0500

    upd: add dynamic height for cinema.html

--- Django/CSUProjects/views.py
+++ Django/CSUProjects/views.py
@@ -73,5 +73,9 @@ def completedProjects(request):
 def cinema(request, comp_project_id):
     name = CompletedProjects.objects.get(comp_project_id=comp_project_id)
     data = WorkersInProject.objects.select_related('worker_id').filter(comp_project_id=comp_project_id)
+    size = 1000
+    for d in data:
+        size += 1500
+    print(size)
     #! TODO решить проблему динамического изменения размера страницы
-    return render(request, 'cinema.html',context= {'nameProject': name, 'cards': data})
+    return render(request, 'cinema.html',context= {'nameProject': name, 'cards': data, 'size': size})
