45751cb0fa868d3b9dbcb1cc496dcfcd39329495
Arseniy <mr.elisar@gmail.com>
2023-11-14 01:23:31 +0500

    upd index, add btn  and form on index page

--- Django/CSUProjects/static/src/JScripts/showSubscriptionForm.js
+++ Django/CSUProjects/static/src/JScripts/showSubscriptionForm.js
@@ -3,6 +3,7 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btns = document.querySelectorAll('.subscribeButton');
 const btnClose = document.querySelector('.icon-close');
+const createBtn = document.querySelector('#create-project');
 
 
 btns.forEach(btn => {
@@ -11,6 +12,9 @@ btns.forEach(btn => {
     });
 })
 
+createBtn.addEventListener('click', ()=> {
+    wrapper.classList.add('active-appear');
+})
 
 btnClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-appear');
