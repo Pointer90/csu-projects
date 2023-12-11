bf77aba1991cfd1c3230679983e558919563b127
Arseniy <mr.elisar@gmail.com>
2023-12-11 18:21:42 +0500

    Исправления: мелкие изменения в фильтрации полей и их отображении

--- Django/CSUProjects/admin.py
+++ Django/CSUProjects/admin.py
@@ -1,33 +1,24 @@
 from django.contrib import admin
 from .models import *
-from django import forms
-
+from .modelsInline import SubProjectNeedsInline, SubProjectsInline
 # Register your models here.
 
-class SubProjectsInline(admin.TabularInline):
-    model = SubProjects
-    extra = 0
-
-    fields = ('subprj_name', 'status', 'subprj_preview', 'subprj_description')
-    formfield_overrides = {
-        models.TextField: {'widget': forms.Textarea(attrs={'rows': 10, 'cols': 40})},
-    }
 
 
 @admin.register(Projects)
 class Projects(admin.ModelAdmin):
 
-    list_display = ['prj_name', 'prj_description', 'prj_created', 'status']
-    list_filter = ('prj_created', )
+    list_display = ['prj_name', 'status', 'display_year', ]
+    list_filter = ['status',]
+    search_fields = ['prj_created', 'prj_name__startswith', 'status']
 
     fields = (('prj_name', 'status'), 'prj_preview', 'prj_description')
 
     inlines = [SubProjectsInline]
 
-
+@admin.register(SubProjects)
 class SubProjects(admin.ModelAdmin):
     list_filter = ('subprj_created',)
-
     list_display = ['prj_id',
                     'subprj_name',
                     'subprj_description',
@@ -35,6 +26,8 @@ class SubProjects(admin.ModelAdmin):
                     ]
 
     fields = (('subprj_name', 'status'), 'subprj_preview', 'subprj_description')
+
+    inlines = [SubProjectNeedsInline,]
 
 
 @admin.register(SubProjectNeeds)
@@ -44,20 +37,18 @@ class SubProjectNeeds(admin.ModelAdmin):
 
     fields = ('subprj_id', 'need_profiles', 'need_description')
 
-
-
 @admin.register(Workers)
 class Workers(admin.ModelAdmin):
     list_display = ['worker_full_name']
+    search_fields = ['worker_full_name__startswith',]
 
 
 @admin.register(WorkersInProject)
 class WorkersInProject(admin.ModelAdmin):
-    list_filter = ('worker_post',)
+    list_filter = ('worker_post', 'prj_id')
     list_display = ['worker_id',
                     'worker_post',
                     'prj_id',
-                    'worker_description'
                     ]
     fieldsets = (
         ('Данные работника', {
