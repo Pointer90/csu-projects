95f4616ee893e6a856f6be8d1ad55fde18bd831b
Felix Zeves <karpukov2002@mail.ru>
2024-01-26 17:27:58 +0500

    create projects model

--- Django/CSUProjects/admin.py
+++ Django/CSUProjects/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import *
+from .models import Projects
 
 # Register your models here.
+@admin.register(Projects)
+class Projects(admin.ModelAdmin):
 
+    list_display = ['title', 'status', 'display_year', ]
+    list_filter = ['status',]
+    search_fields = ['creation_date', 'title__startswith', 'status']
+
+    fields = (('title', 'status'), 'photo', 'description')
 
--- Django/CSUProjects/models.py
+++ Django/CSUProjects/models.py
@@ -1,1 +1,54 @@
 from django.db import models
+
+ProjectStatusesEnum = (
+    ('completed', 'Выполнен'),
+    ('quickly', 'Срочно'),
+    ('full', 'Набор окончен'),
+    ('process', 'В процессе'),
+    ('frozen', 'Заморожен'),
+)
+
+class Projects(models.Model):
+    pid = models.AutoField(primary_key=True)
+    title = models.CharField(
+        'Название проекта',
+        max_length=80,
+        unique=True,
+        blank=False
+    )
+    description = models.TextField(
+        'Описание проекта',
+        blank=False
+    )
+    photo = models.ImageField(
+        upload_to='previews/',
+        blank=True,
+        help_text='*необязательное поле',
+        verbose_name='Превью проекта'
+    )
+    status = models.CharField(
+        'Статус',
+        max_length=14,
+        choices=ProjectStatusesEnum,
+        default='process',
+        help_text='Ведите состояние (статус) проекта. По умолчанию статус - В процессе'
+    )
+    creation_date = models.DateTimeField(
+        'Дата создания',
+        auto_now_add=True
+    )
+    updation_date = models.DateTimeField(
+        'Дата изменения',
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+    
+    def display_year(self):
+        return self.creation_date.year
+    display_year.short_description = 'Год создания'
