e5df038fdd4153a9f406815bcbfd8e4ce2a72164
Arseniy <mr.elisar@gmail.com>
2024-01-30 16:54:51 +0500

    upd: TabularInline in admin panel

--- Django/CSUProjects/admin.py
+++ Django/CSUProjects/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Projects, Workers, Subprojects, Vacancies, WorkersInSubprojects
+from .models import Projects, Workers, Subprojects, Vacancies, WorkersInSubprojects, WorkersInSubprojects
+
+class SubprojectsInline(admin.TabularInline):
+    model = Subprojects
+    extra = 0
+
+    fields = ('title', 'status', 'description')
+
+class VacanciesInline(admin.TabularInline):
+    model = Vacancies
+    extra = 0
+
+    fields = ('post', 'description')
+
+class WorkersInSubprojectsInlines(admin.TabularInline):
+    model = WorkersInSubprojects
+    extra = 0
 
 # Register your models here.
 @admin.register(Projects)
@@ -10,6 +26,7 @@ class Projects(admin.ModelAdmin):
     search_fields = ['creation_date', 'title__startswith', 'status']
 
     fields = (('title', 'status'), 'photo', 'description')
+    inlines = [SubprojectsInline]
 
 @admin.register(Workers)
 class Workers(admin.ModelAdmin):
@@ -26,15 +43,16 @@ class SubProjects(admin.ModelAdmin):
                     ]
 
     fields = (('title', 'status'), 'photo', 'description')
+    inlines = [VacanciesInline, WorkersInSubprojectsInlines]
 
-@admin.register(Vacancies)
+# @admin.register(Vacancies)
 class SubProjectNeeds(admin.ModelAdmin):
     list_filter = ('post',)
     list_display = ['sid', 'post', 'description']
 
     fields = ('sid', 'post', 'description')
 
-@admin.register(WorkersInSubprojects)
+# @admin.register(WorkersInSubprojects)
 class WorkersInSubprojects(admin.ModelAdmin):
 
     list_display = ['sid', 'wid', 'post', 'description']
