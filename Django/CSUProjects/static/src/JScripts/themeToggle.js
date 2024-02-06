e1d5131934641358983afd17d332fccbad0fe510
Felix Zeves <karpukov2002@mail.ru>
2024-02-06 17:50:43 +0500

    upd: index, subProjects, compProjects.html: fix: themeToggle.js

--- Django/CSUProjects/static/src/JScripts/themeToggle.js
+++ Django/CSUProjects/static/src/JScripts/themeToggle.js
@@ -2,14 +2,17 @@ const lightTheme = document.querySelector('button#lightBtn');
 const darkTheme = document.querySelector('button#darkBtn');
 const bodyTag = document.querySelector('[data-tag="body"]');
 const svgs = document.querySelectorAll('.svgObj');
-console.log(svgs);
+const btns = document.querySelectorAll('.lastBtn');
 
 lightTheme.addEventListener("click", function () {
     lightTheme.setAttribute('class', "dropdown-item active rounded-3");
     darkTheme.setAttribute('class', "dropdown-item rounded-3");
     bodyTag.setAttribute('data-bs-theme', "light");
     svgs.forEach(svg => {
-        svg.setAttribute("fill", "white")
+        svg.setAttribute('fill', "white");
+    });
+    btns.forEach(btn =>{
+        btn.setAttribute('class', "btn btn-outline-dark rounded-3 lastBtn");
     });
 });
 
@@ -19,6 +22,8 @@ darkTheme.addEventListener("click", function () {
     bodyTag.setAttribute('data-bs-theme', "dark");
     svgs.forEach(svg => {
         svg.setAttribute("fill", "#212529");
-        console.log(svg);
+    });
+    btns.forEach(btn =>{
+        btn.setAttribute('class', "btn btn-outline-light rounded-3 lastBtn");
     });
 });
