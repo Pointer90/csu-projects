4feb2876ecfb6c0ae0b812639308c3a736356914
Felix Zeves <karpukov2002@mail.ru>
2024-02-06 15:31:18 +0500

    upd: index.html, themeToggle.js - fix svg color

--- Django/CSUProjects/static/src/JScripts/themeToggle.js
+++ Django/CSUProjects/static/src/JScripts/themeToggle.js
@@ -1,15 +1,24 @@
 const lightTheme = document.querySelector('button#lightBtn');
 const darkTheme = document.querySelector('button#darkBtn');
 const bodyTag = document.querySelector('[data-tag="body"]');
+const svgs = document.querySelectorAll('.svgObj');
+console.log(svgs);
 
 lightTheme.addEventListener("click", function () {
     lightTheme.setAttribute('class', "dropdown-item active rounded-3");
     darkTheme.setAttribute('class', "dropdown-item rounded-3");
     bodyTag.setAttribute('data-bs-theme', "light");
+    svgs.forEach(svg => {
+        svg.setAttribute("fill", "white")
+    });
 });
 
 darkTheme.addEventListener("click", function () {
     lightTheme.setAttribute('class', "dropdown-item rounded-3");
     darkTheme.setAttribute('class', "dropdown-item active rounded-3");
     bodyTag.setAttribute('data-bs-theme', "dark");
+    svgs.forEach(svg => {
+        svg.setAttribute("fill", "#212529");
+        console.log(svg);
+    });
 });
