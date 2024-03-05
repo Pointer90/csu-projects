d3e0fdae3c843a7945415c59f0ebced62340d14d
Arseniy <mr.elisar@gmail.com>
2024-03-05 19:00:54 +0500

    fix: fonst, weight

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -21,9 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.getenv('SECRET_KEY')
 SECRET_KEY = 'django-insecure-t0c&uj*!-8w6l@ws3(yv(qk1ujsn1h*f1hkv*kinhhdh)d5tp)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = os.getenv('DEBUG')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
@@ -107,8 +109,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+# LANGUAGE_CODE = os.getenv('LANGUAGE_CODE')
 LANGUAGE_CODE = 'ru'
 
+# TIME_ZONE = os.getenv('TIME_ZONE')
 TIME_ZONE = 'Asia/Yekaterinburg'
 
 USE_I18N = True
