5931ca8ca74c6ce104e7e36c436d158320ca2b16
Felix Zeves <karpukov2002@mail.ru>
2024-06-09 13:32:36 +0500

    fix: clear code, delete useless const

--- Django/CSUProjects/admin.py
+++ Django/CSUProjects/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django import forms
 
 from django.db import models
-from .models import Projects, Workers, Subprojects, Vacancies, WorkersInSubprojects, WorkersInSubprojects
+from .models import Projects, Workers, Subprojects, Vacancies, WorkersInSubprojects
 
 # TabularInlines
 
