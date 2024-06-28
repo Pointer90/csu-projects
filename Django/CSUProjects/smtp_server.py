db1088cdddc96747fa5dffd9f206b2a2bfaa4c40
Arseniy Eliseev <mr.elisar@gmail.com>
2024-06-28 23:14:16 +0500

    upd: add smtp env var

--- Django/CSUProjects/smtp_server.py
+++ Django/CSUProjects/smtp_server.py
@@ -2,25 +2,16 @@ import smtplib
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from os import environ
+from os import getenv
 
 def send_form(data:dict) -> None:
 
     try:
-
-        # Формирую шапку письма
-        # email_from = environ.get("EMAIL_FROM")
-        # email_to = environ.get("EMAIL_TO")
-        # token_access = environ.get("EMAIL_TOKEN_ACCESS")
-        # smtp_server = environ.get("SMTP_SERVER")
-        # smtp_port = environ.get("SMTP_PORT")
-
-        # Todo: Убрать шаблоны
-        email_from = "user1@example.com"
-        email_to = "user1@example.com"
-        token_access = "rs1fz1knh1wc1xoy"
-        smtp_server = "smtp.example.ru"
-        smtp_port = 465
+        email_from = getenv('EMAIL_FROM')
+        email_to = getenv('EMAIL_TO')
+        token_access = getenv('EMAIL_TOKEN_ACCESS')
+        smtp_server = getenv('SMTP_SERVER')
+        smtp_port = getenv('SMTP_PORT')
 
         msg = MIMEMultipart()
         msg['From'] = email_from
@@ -38,11 +29,9 @@ def send_form(data:dict) -> None:
         server.login(email_from, token_access)
         text = msg.as_string()
 
-        # Отправка письма на почту себе и получателю
         server.sendmail(email_from, email_to, text)
 
     except Exception as e:
-        # Todo: Добавить обработчик
         print(e)
     finally:
         server.quit()
